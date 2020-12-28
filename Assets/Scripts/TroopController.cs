66b380c4643acc900a30ddc29c893c3f3c32833f
Skylar Masson <smasson1998@gmail.com>
2020-12-27 23:11:14 -0600

    Started troops controller

---
+++ Assets/Scripts/TroopController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TroopController : MonoBehaviour {
+    
+    public float speed = 2;
+    public float moveDist = 5;
+    public float attackDist = 5;
+
+    Animator anim;
+    Transform target;
+    Rigidbody body;
+
+    public enum State {
+        Idle,
+        Move,
+        Attack,
+        Dead
+    }
+
+    public State state = State.Idle;
+
+    void Awake() {
+        body = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        target = GameObject.FindGameObjectsWithTag("Target").transform;
+    }
+
+    void Update() {
+        switch (case) {
+            case State.Idle:
+                IdleUpdate();
+                break;
+            case State.Move:
+                MoveUpdate();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void IdleUpdate() {
+        //Set animation bool to false
+        //body velocity = zero
+        //distance = vector3 distance
+        //if dist < moveDist state is Chase
+    }
+
+    void MoveUpdate() {
+        //A lot goes here
+    }
+
+    IEnumerator Shoot() {
+        //Ill attack when at a certain distance
+    }
+}
