f09fb66e53430002647daf2f59a08b7081698f7b
Skylar Masson <smasson1998@gmail.com>
2020-12-27 22:03:46 -0600

    Creating event trigger for TroopsUI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    
+    public static GameManager instance; //Creating GameManager singleton
+
+    public Animator troopsUIAnimator;
+
+    bool troopsUIOpen = false;
+
+    void Awake() {
+        if (instance == null) instance = this;
+    }
+
+    public void TroopsUIHover() {
+        if (troopsUIOpen == false) {
+            troopsUIAnimator.SetBool("Hovered", true);
+            troopsUIOpen = true;
+            TroopsUIHovered();
+        }
+    }
+    
+    public void TroopsUIExit() {
+        if (troopsUIOpen == true) {
+            troopsUIOpen = false;
+            troopsUIAnimator.SetBool("Hovered", false);
+        }
+    }
+
+    public void TroopsUIHovered() {
+        troopsUIOpen = true;
+    }
+
+    public void TroopsUIClosed() {
+        troopsUIOpen = true;
+        TroopsUIExit();
+    }
+}
