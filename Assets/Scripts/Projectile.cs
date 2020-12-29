6cf4083824a3b33888d0e9a21c957cd293e177ca
Skylar Masson <smasson1998@gmail.com>
2020-12-28 19:11:32 -0600

    Troops bullets now damage enemies

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
     
     public float speed;
+    public float attackDamage = 10;
 
     void Update() {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    
+    void OnTriggerEnter(Collider c) {
+        HitObject(c.gameObject);
+        this.gameObject.SetActive(false);
+    }
+
+    void HitObject(GameObject g) {
+        HealthController health = g.GetComponent<HealthController>();
+        if (health != null) {
+            health.TakeDamage(attackDamage);
+        }
     }
 }
