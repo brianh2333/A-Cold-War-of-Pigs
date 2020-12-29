1288a70003b3bc500e83fa75cba036ea980dd452
Skylar Masson <smasson1998@gmail.com>
2020-12-29 16:36:53 -0600

    Main menu complete

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+    
+    public GameObject middlePanel;
+    public GameObject instructionsPanel;
+    public GameObject creditsPanel;
+
+    public void Start() {
+        middlePanel.SetActive(true);
+        instructionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Instructions() {
+        middlePanel.SetActive(false);
+        instructionsPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+    }
+
+    public void Credits() {
+        middlePanel.SetActive(false);
+        instructionsPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void Back() {
+        middlePanel.SetActive(true);
+        instructionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
